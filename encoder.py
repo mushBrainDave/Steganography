0112c4a57a949c4457e56fae5d29d627d3fa1f96
mushBrainDave <dswanson@unomaha.edu>
2020-06-24 21:58:30 -0500

    Simplified input text storage
    greatly simplified CreateMessage

--- encoder.py
+++ encoder.py
@@ -4,53 +4,35 @@ from PIL import Image
 
 
 def CreateMessage(message):
-    ascii = 7
-
-    message_stream = message.getbuffer()
-    bit_array = []
-    #bit_array = [0 for x in range(len(message_stream) * 7)]
-    #print(bit_array)
-    for letters in range(len(message_stream)):
-        for bits in range(ascii):
-            bit = message_stream[letters] >> bits
-            if bit % 2 == 0:
-                bit_array.append(0)
-            else:
-                bit_array.append(1)
-    print(bit_array)
-    return bit_array
+    temp = bin(int.from_bytes(message.encode(), 'big'))[2:]  # removes 0b from beginning
+    string = temp.zfill(len(temp)+1)  # adds 0 to beginning
+    return string
 
 
-def Encode(picture, bit_array):
+def Encode(picture, string):
     pixel_byte = picture.tobytes()
     pixel_stream = BytesIO(pixel_byte)  # creates stream with bytes from pixel
     pixel_buffer = pixel_stream.getbuffer()  # turns stream into a memoryview that is mutable and consistent
-    for rgb in range(len(bit_array)):
-        if pixel_buffer[rgb] % 2 == 0 and bit_array[rgb] == 1:
-            pixel_buffer[rgb] += 1
-        elif pixel_buffer[rgb] % 2 == 1 and bit_array[rgb] == 0:
-            pixel_buffer[rgb] -= 1
-        else:
-            pass
+    s = "0"
+    for index, bit in enumerate(string):
+        if pixel_buffer[index] % 2 == 0 and int(bit) == 1:
+            pixel_buffer[index] += 1
+        elif pixel_buffer[index] % 2 == 1 and int(bit) == 0:
+            pixel_buffer[index] -= 1
+
     width, height = picture.size
     encoded_bytes = pixel_buffer.tobytes()
-    print(encoded_bytes[0])
     encoded_picture = Image.frombytes("RGB", (width, height), encoded_bytes)
     return encoded_picture
 
 
 def main():
     picture = Image.open(r"C:\Users\Mushbrain\Desktop\untitled1.jpg")
-    message = BytesIO(b"hello world")
-    bit_array = CreateMessage(message)
-    encoded_picture = Encode(picture, bit_array)
-    print(encoded_picture.getpixel((0, 0)))
-
+    message = "hello world"
+    string = CreateMessage(message)
+    encoded_picture = Encode(picture, string)
     encoded_picture.save(r"C:\Users\Mushbrain\Desktop\fromb.jpg", subsampling=0, quality=100)
     encoded_picture.save(r"C:\Users\Mushbrain\Desktop\fromb.png")
-
-    picture1 = Image.open(r"C:\Users\Mushbrain\Desktop\fromb.png")
-    print(picture1.getpixel((0, 0)))
 
 
 if __name__ == "__main__":
