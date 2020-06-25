55523d47a345eb33dfd868e562e8f082be6af2a7
mushBrainDave <dswanson@unomaha.edu>
2020-06-24 22:00:01 -0500

    Is working
    may want to modify the one line wonder at 30 for readability

--- decoder.py
+++ decoder.py
@@ -7,42 +7,37 @@ def DecodeMessage(picture):
     pixel_byte = picture.tobytes()
     pixel_stream = BytesIO(pixel_byte)  # creates stream with bytes from pixel
     pixel_buffer = pixel_stream.getbuffer()  # turns stream into a memoryview that is mutable and consistent
-    bit_array = []
     string = ""
-    print(pixel_byte[3])
-    for letters in range(len(pixel_buffer)):
-        bit = pixel_buffer[letters] >> 1
-        #print(pixel_buffer[letters])
-        if bit % 2 == 0:
+    byte_array = []
+    for index, letter in enumerate(pixel_buffer):
+        if index > 100:
+            break
+        elif index % 8 == 0:
+            byte_array.append(string)
+            string = ""
+        if letter % 2 == 0:
             string += str(0)
-            bit_array.append(0)
-        else:
+        elif letter % 2 == 1:
             string += str(1)
-            bit_array.append(1)
-    #print(bit_array)
-
-    return bit_array, string
+    byte_array.pop(0)
+    return byte_array
 
 
-def CompileMessage(bit_array, string):
-    print("")
-#    s = ["00100001"]
-#    print(type(s[0]))
-#    s1 = int(s[0], 2)
-#    print(s1)
-#    s2 = (97).to_bytes(1, byteorder="little")
-#    print(s2.decode("ascii"))
-#    for x in range(byte_array):
-#        for byt in range(7):
-            
+def CompileMessage(byte_array):
+    message = ''
+    try:
+        for byte in byte_array:
+            message += str(int(byte, 2).to_bytes(1, byteorder="little").decode("ascii"))
+    except UnicodeDecodeError:
+        pass
+    return message
 
 
 def main():
-    picture = Image.open(r"C:\Users\Mushbrain\Desktop\fromb.jpg")
-    print(picture.getpixel((0, 0)))
-
-    byte_array, string = DecodeMessage(picture)
-    CompileMessage(byte_array, string)
+    picture = Image.open(r"C:\Users\Mushbrain\Desktop\fromb.png")
+    byte_array = DecodeMessage(picture)
+    message = CompileMessage(byte_array)
+    print(message)
 
 
 if __name__ == "__main__":
