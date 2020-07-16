19e45a5f59fa7a513bc1e6c08966b1d2814d615f
mushBrainDave <dswanson@unomaha.edu>
2020-07-15 19:46:52 -0500

    update user submission to include file

--- mysite/client/views.py
+++ mysite/client/views.py
@@ -20,31 +20,23 @@ class IndexView(generic.ListView):
         return Message.objects.order_by('-id')[:5]
 
 
-class DetailView(generic.DetailView):
-    model = Message
-    template_name = 'client/detail.html'
-
-
 def message(request, message_id):
     message = get_object_or_404(Message, pk=message_id)
     return render(request, 'client/detail.html', {'message': message})
 
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
+def hotel_image_view(request):
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
+        form = NameForm(request.POST, request.FILES)
+
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
             form.save()
             return HttpResponseRedirect('/client/')
-
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
+    return render(request, 'client/image_form.html', {'form': form})
 
-    return render(request, 'client/name.html', {'form': form})
+
+def success(request):
+    return HttpResponse('successfully uploaded')
+
