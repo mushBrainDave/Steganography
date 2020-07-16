0d08983ac6fe4adb090347893bc590dccb51521e
mushBrainDave <dswanson@unomaha.edu>
2020-07-15 19:44:11 -0500

    updated url
    # TO DO update view hotel which was a c/p from tutorial

--- mysite/client/urls.py
+++ mysite/client/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+#from .views import hotel_image_view
 
 app_name = 'client'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:message_id>/', views.message, name='message'),
-    path('name/', views.get_name, name='name')
+    path('image_upload', views.hotel_image_view, name='image_form'),
+
 ]
